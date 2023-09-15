6694135e8f059e2ef1aa2ebe3e284c4913610e48
Sheikh <smabid2018@gmail.com>
2023-09-16 04:51:14 +0545

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 import { LoginReg } from './Components/LoginReg/LoginReg';
 
 function App() {
+
+  const [lat,setLat]=useState()
+  const [lon,setLon]=useState()
+
+  useEffect(()=>{
+      navigator.geolocation.getCurrentPosition((postion)=>{
+          setLat(postion.coords.latitude)
+          setLon(postion.coords.longitude)
+      })
+  })
+
+
   return (
     <div>
       <LoginReg/>
+      {console.log(lat)}
+      {console.log(lon)}
     </div>
   );
 }
